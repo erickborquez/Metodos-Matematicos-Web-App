f2910747fb91d5a65404c5ee749bf7ecfb9aac1f
ErickJoestar <31946895+ErickJoestar@users.noreply.github.com>
2019-11-07 00:18:59 -0600

    Minor bugs fixed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Feed from './Feed';
 import '../Styles/Home.css'
 import Header from './Header';
-import UserOptions from './UserOptions';
-
 
 const Home = () => {
     return (
--- src/Components/Side.js
+++ src/Components/Side.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import '../Styles/Side.css'
 
 import CreateTeam from './CreateTeam';
 import TeamCard from './TeamCard';
+
+import '../Styles/Side.css'
+
 
 import background1 from '../assets/background-1.jpg';
 import background2 from '../assets/background-2.jpg';
@@ -11,18 +13,17 @@ import background4 from '../assets/background-4.jpg';
 
 
 const IMAGES = [
-    { src: background1, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
-    { src: background2, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
-    { src: background3, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
-    { src: background4, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
+    { id: 1, src: background1, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
+    { id: 2, src: background2, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
+    { id: 3, src: background3, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
+    { id: 4, src: background4, alt: "trash", title: "Recoleccion de basura", subtitle: "Siguiente tarea: 16/11/19" },
 ];
 
 
 const Side = (props = {}) => {
     return (
         <section className="side">
-            {props.teams}
-            {IMAGES.map(img => <TeamCard src={img.src} alt={img.alt} title={img.title} subtitle={img.subtitle} />)}
+            {IMAGES.map(img => <TeamCard key={img.id} src={img.src} alt={img.alt} title={img.title} subtitle={img.subtitle} />)}
             <CreateTeam className="side_create-team" />
         </section>
     )
