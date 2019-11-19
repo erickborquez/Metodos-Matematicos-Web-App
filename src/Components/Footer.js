b914de41203cd0ba07cc58314306612e9cb60700
ErickJoestar <31946895+ErickJoestar@users.noreply.github.com>
2019-11-19 00:28:26 -0600

    Footer initialized

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../Styles/Footer.css'
+
+const Footer = () =>{
+    return (
+        <footer className="footer">
+
+        </footer>
+    )
+}
+
+export default Footer;
